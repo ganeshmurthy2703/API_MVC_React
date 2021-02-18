99714c59be8f63989c9d4bed0a8f688f3d227f20
ganeshmurthy2703 <ganeshmurthy2027@gmail.com>
2021-02-18 11:49:27 -0500

    MVC Frontend client created with ASP.NET Core

--- MVC_Frontend/Startup.cs
+++ MVC_Frontend/Startup.cs
@@ -1,17 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SongsAPI
+namespace MVC_Frontend
 {
     public class Startup
     {
@@ -25,23 +23,7 @@ namespace SongsAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
-            {
-                options.AddPolicy("AllowEverything",
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                                .AllowAnyMethod()
-                                .AllowAnyHeader();
-                    });
-            });
-
-            // services.AddCors(options =>
-            //     options.AddPolicy("AllowSpecific", p => p.WithOrigins("http://localhost:5000")
-            //                                             .WithMethods("GET")
-            //                                             .WithHeaders("name")));
-
-            services.AddControllers();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,18 +33,24 @@ namespace SongsAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
-            //~dk
-            //app.UseAuthorization();
-            //app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCors("AllowEverything");
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
