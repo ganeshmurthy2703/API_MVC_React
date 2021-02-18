d17996fedf8bad5a931adef1f75ab442dfe3e0ba
ganeshmurthy2703 <ganeshmurthy2027@gmail.com>
2021-02-18 14:13:50 -0500

    Cleaning up Reaact Code

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react'; 
 import { Navbar, Nav, Form, NavDropdown, FormControl, Button } from 'react-bootstrap';
